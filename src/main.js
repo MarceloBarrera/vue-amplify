e051f41a61423a9ca48dbee66513b4de716d6043
MarceloBarrera <marcelobarrera@048-WSLT.local>
2021-07-21 08:26:52 +0100

    adding authentication functionality

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 import WaveUI from 'wave-ui'
 import 'wave-ui/dist/wave-ui.css'
+
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
+
 Vue.config.productionTip = false
 
 Vue.use(WaveUI)
